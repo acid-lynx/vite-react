edf739e85aba1d44c4c08b3b28a275288a259be1
acid.lynx <bartolomeos.v93@gmail.com>
2025-12-11 16:05:19 +0200

    tailwind setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,14 @@
 import clsx from 'clsx'
 import { Link, Outlet } from 'react-router-dom'
 import { useTheme } from '@/app/providers/ThemeProvider'
-
-import './styles/index.scss'
+import './styles/index.css'
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme()
 
     return (
         <div className={ clsx('app', theme) }>
-            <div className="text-3xl">
+            <div className="text-5xl">
                 <button onClick={ toggleTheme }>TOGGLE</button>
                 <Link to={ '/' }>Главная</Link>
                 <Link to={ '/about' }>О сайте</Link>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      tailwindcss(),
+  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
